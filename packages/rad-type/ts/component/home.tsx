1b645f3f88be1e677e02bfae7a8e934b58f7f494
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2019-11-08 06:16:42 -0800

    Allow visualizations to be toggled.

--- packages/rad-type/ts/component/home.tsx
+++ packages/rad-type/ts/component/home.tsx
@@ -5,16 +5,29 @@ import * as Emotion from "emotion";
 import * as RadType from "rad-type";
 
 const emo = {
-  row: (heightPx: number) => Emotion.css`
+  button: (fontSize: number) => Emotion.css`
+    ${RadType.emo.text(fontSize)};
+    padding-left: 5px;
+    padding-right: 5px;
+    margin-left: 5px;
+    margin-right: 5px;
+  `,
+
+  row: (paddingPx: number) => Emotion.css`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    padding-top: ${paddingPx}px;
+    padding-bottom: ${paddingPx}px;
+  `,
+  rowHeighted: (heightPx: number, paddingPx: number) => Emotion.css`
+    ${emo.row(heightPx)};
     height: ${heightPx}px;
   `,
 
-  rowWidthed: (widthPx: number, heightPx: number) => Emotion.css`
-    ${emo.row(heightPx)};
+  rowWidthed: (widthPx: number, paddingPx: number) => Emotion.css`
+    ${emo.row(paddingPx)};
     width: ${widthPx}px;
   `,
 
@@ -48,7 +61,8 @@ export const Home = () => {
   const gamepadDotDiameterPx = 5;
   const gamepadDotRadiusRation = gamepadDotDiameterPx / bigCircleDiameterPx;
   const rowWidthPx = 1050;
-  const rowHeightPx = 700;
+  const rowHeightPx = 100;
+  const rowPaddingPx = 50;
 
   const [gamepadId, setGamepadId] = React.useState<number | undefined>();
 
@@ -71,120 +85,142 @@ export const Home = () => {
       window.removeEventListener("gamepaddisconnected", eventListener);
   });
 
-  const [text0, setText0] = React.useState("");
-  const appendLetter0 = React.useCallback(
-    (letter: string) => setText0(t => `${t}${letter}`),
-    [setText0],
+  const [text, setText] = React.useState("");
+  const appendLetter = React.useCallback(
+    (letter: string) => setText(t => `${t}${letter}`),
+    [setText],
   );
-  const backspace0 = React.useCallback(() => setText0(t => t.slice(0, -1)), [
-    setText0,
+  const backspace = React.useCallback(() => setText(t => t.slice(0, -1)), [
+    setText,
   ]);
-  const appendSpace0 = React.useCallback(() => appendLetter0(" "), [
-    appendLetter0,
+  const appendSpace = React.useCallback(() => appendLetter(" "), [
+    appendLetter,
   ]);
 
-  RadType.useButtonEvents(gamepadId, 2, undefined, backspace0);
-  RadType.useButtonEvents(gamepadId, 3, undefined, appendSpace0);
+  RadType.useButtonEvents(gamepadId, 2, undefined, backspace);
+  RadType.useButtonEvents(gamepadId, 3, undefined, appendSpace);
 
-  const [text1, setText1] = React.useState("");
-  const appendLetter1 = React.useCallback(
-    (letter: string) => setText1(t => `${t}${letter}`),
-    [setText1],
+  const [visualizationId, setVisualizationId] = React.useState(0);
+  const toggleVisualizationId = React.useCallback(
+    () =>
+      setVisualizationId(visualizationId => (visualizationId === 0 ? 1 : 0)),
+    [],
   );
-  const backspace1 = React.useCallback(() => setText1(t => t.slice(0, -1)), [
-    setText1,
-  ]);
-  const appendSpace1 = React.useCallback(() => appendLetter1(" "), [
-    appendLetter1,
-  ]);
 
-  RadType.useButtonEvents(gamepadId, 2, undefined, backspace1);
-  RadType.useButtonEvents(gamepadId, 3, undefined, appendSpace1);
+  const clearText = React.useCallback(() => setText(""), []);
+
+  const visualization = React.useMemo(
+    () =>
+      visualizationId === 0 ? (
+        <>
+          <RadType.RadTypeVis
+            boxSizePx={bigCircleDiameterPx}
+            lineThicknessPx={lineThicknessPx}
+            fontSize={fontSize}
+            centerKey={"E"}
+            keys={["F", "V", "G", "R", "W", "Q", "A", "S", "Z", "X", "C", "D"]}
+            targetCircleRadiusRation={targetCircleRadiusRation}
+            letterCircleRadiusRation={letterCircleRadiusRation}
+            tinyCircleRadiusRation={tinyCircleRadiusRation}
+            gamepadDotRadiusRation={gamepadDotRadiusRation}
+            angleOffsetMultiplier={ringOffsetMultiplier}
+            gamepadId={gamepadId}
+            xAxisId={0}
+            yAxisId={1}
+            altButtonId={4}
+            appendLetter={appendLetter}
+          />
+          <RadType.RadTypeVis
+            boxSizePx={bigCircleDiameterPx}
+            lineThicknessPx={lineThicknessPx}
+            fontSize={fontSize}
+            centerKey={"T"}
+            keys={["L", "P", "O", "I", "U", "Y", "J", "H", "B", "N", "M", "K"]}
+            targetCircleRadiusRation={targetCircleRadiusRation}
+            letterCircleRadiusRation={letterCircleRadiusRation}
+            tinyCircleRadiusRation={tinyCircleRadiusRation}
+            gamepadDotRadiusRation={gamepadDotRadiusRation}
+            angleOffsetMultiplier={ringOffsetMultiplier}
+            gamepadId={gamepadId}
+            xAxisId={2}
+            yAxisId={3}
+            altButtonId={5}
+            appendLetter={appendLetter}
+          />
+        </>
+      ) : (
+        <>
+          <RadType.RadTypeVisEx
+            boxSizePx={bigCircleDiameterPx}
+            lineThicknessPx={lineThicknessPx}
+            fontSize={fontSize}
+            centerKey={"E"}
+            defaultKeys={["F", "G", "R", "W", "A", "S", "D", "C"]}
+            altKeys={["V", "Q", "Z", "X"]}
+            targetCircleRadiusRation={targetCircleRadiusRation}
+            letterCircleRadiusRation={letterCircleRadiusRation}
+            altLetterCircleRadiusRation={altLetterCircleRadiusRation}
+            tinyCircleRadiusRation={tinyCircleRadiusRation}
+            gamepadDotRadiusRation={gamepadDotRadiusRation}
+            gamepadId={gamepadId}
+            xAxisId={0}
+            yAxisId={1}
+            altButtonId={4}
+            appendLetter={appendLetter}
+          />
+          <RadType.RadTypeVisEx
+            boxSizePx={bigCircleDiameterPx}
+            lineThicknessPx={lineThicknessPx}
+            fontSize={fontSize}
+            centerKey={"T"}
+            defaultKeys={["O", "I", "U", "Y", "H", "N", "M", "L"]}
+            altKeys={["P", "K", "J", "B"]}
+            targetCircleRadiusRation={targetCircleRadiusRation}
+            letterCircleRadiusRation={letterCircleRadiusRation}
+            altLetterCircleRadiusRation={altLetterCircleRadiusRation}
+            tinyCircleRadiusRation={tinyCircleRadiusRation}
+            gamepadDotRadiusRation={gamepadDotRadiusRation}
+            gamepadId={gamepadId}
+            xAxisId={2}
+            yAxisId={3}
+            altButtonId={5}
+            appendLetter={appendLetter}
+          />
+        </>
+      ),
+    [
+      altLetterCircleRadiusRation,
+      appendLetter,
+      gamepadDotRadiusRation,
+      gamepadId,
+      letterCircleRadiusRation,
+      ringOffsetMultiplier,
+      targetCircleRadiusRation,
+      tinyCircleRadiusRation,
+      visualizationId,
+    ],
+  );
 
   return (
     <div className={emo.vertical()}>
-      <div className={emo.rowWidthed(rowWidthPx, rowHeightPx)}>
-        <RadType.RadTypeVis
-          boxSizePx={bigCircleDiameterPx}
-          lineThicknessPx={lineThicknessPx}
-          fontSize={fontSize}
-          centerKey={"E"}
-          keys={["F", "V", "G", "R", "W", "Q", "A", "S", "Z", "X", "C", "D"]}
-          targetCircleRadiusRation={targetCircleRadiusRation}
-          letterCircleRadiusRation={letterCircleRadiusRation}
-          tinyCircleRadiusRation={tinyCircleRadiusRation}
-          gamepadDotRadiusRation={gamepadDotRadiusRation}
-          angleOffsetMultiplier={ringOffsetMultiplier}
-          gamepadId={gamepadId}
-          xAxisId={0}
-          yAxisId={1}
-          altButtonId={4}
-          appendLetter={appendLetter0}
-        />
-        <RadType.RadTypeVis
-          boxSizePx={bigCircleDiameterPx}
-          lineThicknessPx={lineThicknessPx}
-          fontSize={fontSize}
-          centerKey={"T"}
-          keys={["L", "P", "O", "I", "U", "Y", "J", "H", "B", "N", "M", "K"]}
-          targetCircleRadiusRation={targetCircleRadiusRation}
-          letterCircleRadiusRation={letterCircleRadiusRation}
-          tinyCircleRadiusRation={tinyCircleRadiusRation}
-          gamepadDotRadiusRation={gamepadDotRadiusRation}
-          angleOffsetMultiplier={ringOffsetMultiplier}
-          gamepadId={gamepadId}
-          xAxisId={2}
-          yAxisId={3}
-          altButtonId={5}
-          appendLetter={appendLetter0}
-        />
+      <div className={emo.rowWidthed(rowWidthPx, rowPaddingPx)}>
+        {visualization}
       </div>
 
-      <div className={emo.row(rowHeightPx)}>
-        <div className={RadType.emo.text(fontSize)}>{text0}</div>
+      <div className={emo.rowHeighted(rowHeightPx, rowPaddingPx)}>
+        <div className={RadType.emo.text(fontSize)}>{text}</div>
       </div>
 
-      <div className={emo.rowWidthed(rowWidthPx, rowHeightPx)}>
-        <RadType.RadTypeVisEx
-          boxSizePx={bigCircleDiameterPx}
-          lineThicknessPx={lineThicknessPx}
-          fontSize={fontSize}
-          centerKey={"E"}
-          defaultKeys={["F", "G", "R", "W", "A", "S", "D", "C"]}
-          altKeys={["V", "Q", "Z", "X"]}
-          targetCircleRadiusRation={targetCircleRadiusRation}
-          letterCircleRadiusRation={letterCircleRadiusRation}
-          altLetterCircleRadiusRation={altLetterCircleRadiusRation}
-          tinyCircleRadiusRation={tinyCircleRadiusRation}
-          gamepadDotRadiusRation={gamepadDotRadiusRation}
-          gamepadId={gamepadId}
-          xAxisId={0}
-          yAxisId={1}
-          altButtonId={4}
-          appendLetter={appendLetter1}
-        />
-        <RadType.RadTypeVisEx
-          boxSizePx={bigCircleDiameterPx}
-          lineThicknessPx={lineThicknessPx}
-          fontSize={fontSize}
-          centerKey={"T"}
-          defaultKeys={["O", "I", "U", "Y", "H", "N", "M", "L"]}
-          altKeys={["P", "K", "J", "B"]}
-          targetCircleRadiusRation={targetCircleRadiusRation}
-          letterCircleRadiusRation={letterCircleRadiusRation}
-          altLetterCircleRadiusRation={altLetterCircleRadiusRation}
-          tinyCircleRadiusRation={tinyCircleRadiusRation}
-          gamepadDotRadiusRation={gamepadDotRadiusRation}
-          gamepadId={gamepadId}
-          xAxisId={2}
-          yAxisId={3}
-          altButtonId={5}
-          appendLetter={appendLetter1}
-        />
-      </div>
-
-      <div className={emo.row(rowHeightPx)}>
-        <div className={RadType.emo.text(fontSize)}>{text1}</div>
+      <div className={emo.row(rowPaddingPx)}>
+        <button
+          className={emo.button(fontSize)}
+          onClick={toggleVisualizationId}
+        >
+          Toggle Visualization
+        </button>
+        <button className={emo.button(fontSize)} onClick={clearText}>
+          Clear Text
+        </button>
       </div>
     </div>
   );
